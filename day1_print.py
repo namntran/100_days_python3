b9a83f841993eed46ba787340d13f9a91005bde1
namntran <namtran78@gmail.com>
2021-01-06 14:47:19 +1000

    string concatenation

--- day1_print.py
+++ day1_print.py
@@ -5,3 +5,7 @@ print("The function is declared like: ")
 print("print('what to print')")
 
 print("Hello world!\nHello world!\nHello world!")
+
+print("Hello" + " Angela")
+
+print("Hello" + " " + "Angela")
